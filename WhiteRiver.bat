7dc7ba5fe4017f745e1d92630ce4ed14d4a4b9fc
PremierITSupport <82167248+PremierITSupport@users.noreply.github.com>
2021-04-14 16:05:12 -0400

    Add files via upload

---
+++ WhiteRiver.bat
@@ -0,0 +1,3 @@
+msiexec.exe /i "C:\Temp\v2\TeamViewer_Host.msi" /qr SETTINGSFILE="C:\Temp\v2\TVoptions.tvopt" DESKTOPSHORTCUTS=0 APITOKEN=12313699-uz1fz2ldK9ixS8wk6bL1 CUSTOMCONFIGID=66way6k
+ping -n 31 127.0.0.1>nul
+"C:\Program Files (x86)\TeamViewer\TeamViewer.exe" assign --api-token 12313699-uz1fz2ldK9ixS8wk6bL1 [--group-id g186343317 --grant-easy-access --reassign]
