6503bdda04523583531cac767998c04eef535444
PremierITSupport <it.support@premierag.com>
2021-04-20 12:14:16 -0400

    Update GenericTVinstall.bat

--- GenericTVinstall.bat
+++ GenericTVinstall.bat
@@ -1,3 +1,3 @@
-msiexec.exe /i "\\PREM-IN-FP1\IT Software\TeamViewer\MSI\Host\TeamViewer_Host.msi" /qr SETTINGSFILE="\\PREM-IN-FP1\IT Software\TeamViewer\MSI\Host\TVoptions.tvopt" DESKTOPSHORTCUTS=0 APITOKEN=9789298-CTqgysXlnAScjw9JhZSf CUSTOMCONFIGID=6bhs8it
+msiexec.exe /i "\\PREM-IN-FP1\IT Software\Teamviewer\MSI\Host\v1\TeamViewer_Host.msi" /qr SETTINGSFILE="\\PREM-IN-FP1\IT Software\Teamviewer\MSI\Host\v1\TVoptions.tvopt" DESKTOPSHORTCUTS=0 APITOKEN=9789298-CTqgysXlnAScjw9JhZSf CUSTOMCONFIGID=6bhs8it
 ping -n 31 127.0.0.1>nul
 "C:\Program Files (x86)\TeamViewer\TeamViewer.exe" assign --api-token 9789298-CTqgysXlnAScjw9JhZSf [--group-id g173258859 --grant-easy-access --reassign]
