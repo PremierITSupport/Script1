3ebd053009363892cc77e9d5678927a29e7d82ad
PremierITSupport <it.support@premierag.com>
2021-07-16 16:10:23 -0400

    Create PSDrivetest03.ps1

---
+++ PSDrivetest03.ps1
@@ -0,0 +1,5 @@
+﻿New-PSDrive -Name source -PSProvider FileSystem -Root \\10.11.6.190\TV | Out-Null
+New-PSDrive -Name target -PSProvider FileSystem -Root 'C:\Temp' | Out-Null
+Copy-Item -Path source:\BitTitanDMASetupPremierAg.msi -Destination target:
+Remove-PSDrive source
+Remove-PSDrive target
