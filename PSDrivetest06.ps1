9331bceb4f95413f42ee70439ff4933eedcab610
PremierITSupport <it.support@premierag.com>
2021-07-19 15:24:06 -0400

    Update PSDrivetest06.ps1

--- PSDrivetest06.ps1
+++ PSDrivetest06.ps1
@@ -1,4 +1,4 @@
-New-Item -ItemType Directory -Force -Path C:\Temp
+New-Item -ItemType Directory -Force -Path C:\Temp | Out-Null
 New-PSDrive -Name source -PSProvider FileSystem -Root \\10.11.6.190\TV | Out-Null
 New-PSDrive -Name target -PSProvider FileSystem -Root 'C:\Temp' | Out-Null
 Copy-Item -Path source:\BitTitanDMASetupPremierAg.msi -Destination target:
