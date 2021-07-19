bcea1befbf267014465d7430bdab763b4ff979b3
PremierITSupport <it.support@premierag.com>
2021-07-19 12:16:17 -0400

    Create PSDrivetest06.ps1

---
+++ PSDrivetest06.ps1
@@ -0,0 +1,7 @@
+New-Item -ItemType Directory -Force -Path C:\Temp
+New-PSDrive -Name source -PSProvider FileSystem -Root \\10.11.6.190\TV | Out-Null
+New-PSDrive -Name target -PSProvider FileSystem -Root 'C:\Temp' | Out-Null
+Copy-Item -Path source:\BitTitanDMASetupPremierAg.msi -Destination target:
+Remove-PSDrive source
+Remove-PSDrive target
+Start-Process msiexec.exe -Wait '/I C:\Temp\BitTitanDMASetupPremierAg.msi'
